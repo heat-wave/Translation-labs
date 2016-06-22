9e6310b1e98cc54ab7bf49080e09f271fa5dcec9
heat-wave <allpicksucks@gmail.com>
2016-06-22 07:25:44 +0300

    Complete task.

--- src/task4/testing/SimpleCode.java
+++ src/task4/testing/SimpleCode.java
@@ -1,8 +1,16 @@
 package task4.testing;
 
-public class SimpleCode {
-    int n = 10;
+import java.util.*;
+
+class SimpleCode {
+
+    int a = 0;
+    static int b = 10;
 
     public static void main(String[] args) {
+        args[0] = null;
+        int c = new Random().nextInt();
+        int d = b % 10;
+        int res = c * (d + 1) - (10 + new Random().nextInt());
     }
 }
